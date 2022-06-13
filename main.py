f8b9fdf708dfe0ef4bc49a8129ca9499e8bd1c90
David <davyu735@gmail.com>
2022-06-13 02:57:31 -0400

    chore: implemented good python design practices

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ import random
 from typing_logic import *
 import time
 
-word_list = []
-player_typing = []
-time_spent = 0
-active_typing = ""
+
+# helper methods
 
 
-def test_length(length, mode):
-    if mode == 1:
+def test_length(length, typing_mode):
+    if typing_mode == 1:
         for i in range(length):
             word_list.append(random.choice(open('python_words.txt').read().split()).strip())
-    elif mode == 2:
+    elif typing_mode == 2:
         for i in range(length):
             word_list.append(random.choice(open('random_words.txt').read().split()).strip())
     return
 
 
-def render_text(list):
+def render_text(typing_list):
     x_index = 50
     y_index = 30
-    for i in range(len(list)):
-        words = font.render(list[i], True, BLACK)
+    for i in range(len(typing_list)):
+        words = font.render(typing_list[i], True, BLACK)
         words_rect = words.get_rect(center=(x_index, y_index))
         screen.blit(words, words_rect)
         x_index += 1000/10
@@ -37,32 +35,21 @@ def results():
     accuracy_rect = accuracy.get_rect(center=(500, 325))
     screen.blit(accuracy, accuracy_rect)
 
-    time = font.render('Time Spent: ' + str(time_spent) + ' seconds', True, BLACK)
-    time_rect = time.get_rect(center=(500, 350))
-    screen.blit(time, time_rect)
+    total_time = font.render('Time Spent: ' + str(time_spent) + ' seconds', True, BLACK)
+    time_rect = total_time.get_rect(center=(500, 350))
+    screen.blit(total_time, time_rect)
 
     wpm = font.render("WPM: " + get_wpm(time_spent, player_typing), True, BLACK)
     wpm_rect = wpm.get_rect(center=(500, 375))
     screen.blit(wpm, wpm_rect)
 
 
-def countdown(seconds):
-    count = seconds
-    for i in range(seconds):
-        countdown = font.render(str(count - i) + "...", True, BLACK)
-        countdown_rect = countdown.get_rect(center=(500, 250))
-        screen.blit(countdown, countdown_rect)
-        time.sleep(1)
-        if count - i == 1:
-            start = font.render("Go!", True, BLACK)
-            start_rect = start.get_rect(center=(500, 250))
-            screen.blit(start, start_rect)
-
-
 successes, failures = pygame.init()
 print("{0} successes and {1} failures".format(successes, failures))
 pygame.key.set_repeat(500, 20)
 pygame.display.set_caption('Monkeytype')
+
+# images
 retry_img = pygame.image.load('images/retry.png')
 surprised_img = pygame.image.load('images/surprised.png')
 monkey_img = pygame.image.load('images/monkey.png')
@@ -72,10 +59,10 @@ monkey2_img = pygame.image.load('images/monkey2.png')
 screen = pygame.display.set_mode((1000, 700))
 clock = pygame.time.Clock()
 FPS = 60
-
 counter, text = 10, '10'.rjust(3)
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 
+# colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -85,7 +72,6 @@ color = RED
 # font
 font = pygame.font.Font(None, 32)
 title = pygame.font.Font(None, 128)
-user_text = ''
 
 # text field
 text_field = pygame.Rect(450, 275, 150, 32)
@@ -99,13 +85,22 @@ retry_button = pygame.Rect(475, 425, 50, 32)
 english_button = pygame.Rect(400, 275, 200, 30)
 python_button = pygame.Rect(400, 350, 200, 30)
 
+# rendering text for buttons
 one_line = font.render("One Line", True, WHITE)
 two_line = font.render("Two Line", True, WHITE)
 three_line = font.render("Three Line", True, WHITE)
-exit = font.render("Exit", True, WHITE)
+exit_text = font.render("Exit", True, WHITE)
 english = font.render("English", True, WHITE)
 python = font.render("Python", True, WHITE)
 
+# variables used for handling typing logic
+word_list = []
+player_typing = []
+time_spent = 0
+active_typing = ""
+user_text = ''
+
+# booleans used for handling typing logic
 active = False
 started = False
 selected = False
@@ -113,8 +108,7 @@ finished = False
 mode_selected = False
 mode = 0
 
-index = 0
-
+# main while loop for the game
 while True:
     clock.tick(FPS)
 
@@ -152,11 +146,6 @@ while True:
                 counter -= 1
                 text = str(counter).rjust(3) if counter > 0 else 'Go!'
             if event.type == pygame.KEYDOWN:
-                # active_typing = user_text[:index + 1]
-                # index += 1
-                # if active_typing != word_list[0][:index + 1]:
-                #     color = RED
-                # print(active_typing)
                 if event.key == pygame.K_BACKSPACE:
                     user_text = user_text[:-1]
                 elif event.key == pygame.K_SPACE:
@@ -175,8 +164,6 @@ while True:
 
     screen.blit(monkey_img, (100, 500))
     screen.blit(monkey2_img, (775, 525))
-    # start_text = font.render('Start', True, BLACK)
-    # screen.blit(start_text, one_line_button)
 
     if mode_selected:
         pygame.draw.rect(screen, WHITE, english_button)
@@ -190,7 +177,7 @@ while True:
         pygame.draw.rect(screen, RED, english_button)
         pygame.draw.rect(screen, RED, python_button)
         pygame.draw.rect(screen, RED, exit_button)
-        screen.blit(exit, exit_button)
+        screen.blit(exit_text, exit_button)
         screen.blit(python, python_button)
         screen.blit(english, english_button)
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -209,7 +196,7 @@ while True:
         screen.blit(one_line, one_line_button)
         screen.blit(two_line, two_line_button)
         screen.blit(three_line, three_line_button)
-        screen.blit(exit, exit_button)
+        screen.blit(exit_text, exit_button)
 
     if started:
         pygame.draw.rect(screen, WHITE, one_line_button)
